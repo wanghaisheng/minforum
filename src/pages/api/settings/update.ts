d349bc52f3a8730e93da09c9ac69bb151974188a
Olalekan Animashaun <kim.olalekan@gmail.com>
2025-02-04 01:16:28 +0100

    up

--- src/pages/api/settings/update.ts
+++ src/pages/api/settings/update.ts
@@ -9,29 +9,37 @@ const update = async (req: NextApiRequest, res: NextApiResponse) => {
     if (auth.success) {
       await Settings.get(req.body.id)
         .then(async (settings: settingsProp) => {
+          let settings: settingsProp = data.length ? data[0] : {};
           let variables: any = settings.extensionVariables || [];
-          let emailSettings = settings.email;
-          let social = settings.socialAccount;
+          let emailSettings = settings?.email;
+          let social = settings?.socialAccount || {};
 
-          social.facebook = enc(social.facebook);
-          social.github = enc(social.github);
-          social.google = enc(social.google);
+          social.facebook = social?.facebook ? enc(social?.facebook) : '';
+          social.github = social?.github ? enc(social?.github) : '';
+          social.google = social?.google ? enc(social?.google) : '';
 
-          const { email, password, host } = emailSettings;
-          emailSettings.password = enc(password);
-          emailSettings.host = enc(host);
-          emailSettings.email = enc(email);
+          if (
+            emailSettings?.email &&
+            emailSettings?.email &&
+            emailSettings?.password
+          ) {
+            const { email, password, host } = emailSettings;
+            emailSettings.password = enc(password);
+            emailSettings.host = enc(host);
+            emailSettings.email = enc(email);
+            settings.email = emailSettings;
+          }
 
-          settings.email = emailSettings;
           settings.cloudflarePublicKey = enc(settings.cloudflarePublicKey);
           settings.cloudflareSecretKey = enc(settings.cloudflareSecretKey);
 
-          variables.forEach((variable: any) => {
-            variable.value = enc(variable.value);
+          variables?.forEach((variable: any) => {
+            variable.value = enc(variable?.value);
           });
 
           settings.extensionVariables = variables;
           settings.socialAccount = social;
+
           await Settings.get(req.body.id).update(req.body);
           res.status(200).json({ success: true, data: 'Updated' });
         })
